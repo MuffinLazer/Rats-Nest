554603a108ed63913b099da5730f75dd4291f6a5
MuffinLazer <muffinlaserr.gmail.com>
2025-01-16 11:13:57 -0500

    Added sentry, Minor changes to map

---
+++ Godot/entities/Sentry/sentry.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var player:CharacterBody2D
+
+@onready var top_half = $SpriteTopHalf
+
+func _process(delta):
+	scale.x = int(player.global_position.x > global_position.x) * 2 - 1
+
+	top_half.look_at(player.global_position)
+	top_half.rotation = clamp(top_half.rotation, -PI/2, PI/4)
